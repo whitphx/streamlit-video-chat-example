40a1946550cd30e898e12211feb1097aabcccc82
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-09-04 14:57:34 +0900

    Update poetry installation in the CI workflows for poetry 1.2.0, Update streamlit-webrtc to 0.43.4 (#102)
    * Update poetry installation in the CI workflows for poetry 1.2.0
    
    * Update the dependency version specification and Add pandas and numpy as dev dependencies to manually resolve the version dependencies
    
    * Specify the click version
    
    * Update app_mcu_*.py to be compatible with streamlit-webrtc>=0.40.0
    
    * Fix not to use the deprecated client_settings option
    
    * Update black

--- app_mcu_simple.py
+++ app_mcu_simple.py
@@ -6,62 +6,55 @@ import av
 import cv2
 import numpy as np
 from streamlit_server_state import server_state, server_state_lock
-from streamlit_webrtc import (
-    ClientSettings,
-    MixerBase,
-    WebRtcMode,
-    create_mix_track,
-    webrtc_streamer,
-)
+from streamlit_webrtc import WebRtcMode, create_mix_track, webrtc_streamer
 from streamlit_webrtc.component import WebRtcStreamerContext
 
 logger = logging.getLogger(__name__)
 
 
-class MultiWindowMixer(MixerBase):
-    def on_update(self, frames: List[av.VideoFrame]) -> av.VideoFrame:
-        buf_w = 640
-        buf_h = 480
-        buffer = np.zeros((buf_h, buf_w, 3), dtype=np.uint8)
+def mixer_callback(frames: List[av.VideoFrame]) -> av.VideoFrame:
+    buf_w = 640
+    buf_h = 480
+    buffer = np.zeros((buf_h, buf_w, 3), dtype=np.uint8)
 
-        n_inputs = len(frames)
+    n_inputs = len(frames)
 
-        n_cols = math.ceil(math.sqrt(n_inputs))
-        n_rows = math.ceil(n_inputs / n_cols)
-        grid_w = buf_w // n_cols
-        grid_h = buf_h // n_rows
+    n_cols = math.ceil(math.sqrt(n_inputs))
+    n_rows = math.ceil(n_inputs / n_cols)
+    grid_w = buf_w // n_cols
+    grid_h = buf_h // n_rows
 
-        for i in range(n_inputs):
-            frame = frames[i]
-            if frame is None:
-                continue
+    for i in range(n_inputs):
+        frame = frames[i]
+        if frame is None:
+            continue
 
-            grid_x = (i % n_cols) * grid_w
-            grid_y = (i // n_cols) * grid_h
+        grid_x = (i % n_cols) * grid_w
+        grid_y = (i // n_cols) * grid_h
 
-            img = frame.to_ndarray(format="bgr24")
-            src_h, src_w = img.shape[0:2]
+        img = frame.to_ndarray(format="bgr24")
+        src_h, src_w = img.shape[0:2]
 
-            aspect_ratio = src_w / src_h
+        aspect_ratio = src_w / src_h
 
-            window_w = min(grid_w, int(grid_h * aspect_ratio))
-            window_h = min(grid_h, int(window_w / aspect_ratio))
+        window_w = min(grid_w, int(grid_h * aspect_ratio))
+        window_h = min(grid_h, int(window_w / aspect_ratio))
 
-            window_offset_x = (grid_w - window_w) // 2
-            window_offset_y = (grid_h - window_h) // 2
+        window_offset_x = (grid_w - window_w) // 2
+        window_offset_y = (grid_h - window_h) // 2
 
-            window_x0 = grid_x + window_offset_x
-            window_y0 = grid_y + window_offset_y
-            window_x1 = window_x0 + window_w
-            window_y1 = window_y0 + window_h
+        window_x0 = grid_x + window_offset_x
+        window_y0 = grid_y + window_offset_y
+        window_x1 = window_x0 + window_w
+        window_y1 = window_y0 + window_h
 
-            buffer[window_y0:window_y1, window_x0:window_x1, :] = cv2.resize(
-                img, (window_w, window_h)
-            )
+        buffer[window_y0:window_y1, window_x0:window_x1, :] = cv2.resize(
+            img, (window_w, window_h)
+        )
 
-        new_frame = av.VideoFrame.from_ndarray(buffer, format="bgr24")
+    new_frame = av.VideoFrame.from_ndarray(buffer, format="bgr24")
 
-        return new_frame
+    return new_frame
 
 
 def main():
@@ -72,7 +65,7 @@ def main():
     with server_state_lock["mix_track"]:
         if "mix_track" not in server_state:
             server_state["mix_track"] = create_mix_track(
-                kind="video", mixer_factory=MultiWindowMixer, key="mix"
+                kind="video", mixer_callback=mixer_callback, key="mix"
             )
 
     mix_track = server_state["mix_track"]
@@ -80,12 +73,8 @@ def main():
     self_ctx = webrtc_streamer(
         key="self",
         mode=WebRtcMode.SENDRECV,
-        client_settings=ClientSettings(
-            rtc_configuration={
-                "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-            },
-            media_stream_constraints={"video": True, "audio": True},
-        ),
+        rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+        media_stream_constraints={"video": True, "audio": True},
         source_video_track=mix_track,
         sendback_audio=False,
     )
@@ -111,12 +100,10 @@ def main():
         webrtc_streamer(
             key=f"sound-{id(ctx)}",
             mode=WebRtcMode.RECVONLY,
-            client_settings=ClientSettings(
-                rtc_configuration={
-                    "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-                },
-                media_stream_constraints={"video": False, "audio": True},
-            ),
+            rtc_configuration={
+                "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+            },
+            media_stream_constraints={"video": False, "audio": True},
             source_audio_track=ctx.input_audio_track,
             desired_playing_state=ctx.state.playing,
         )
